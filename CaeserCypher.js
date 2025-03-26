326fe89dd63369e69709571ca3c6c4ffd357d46c
XtianFreolo <f60@umbc.edu>
2025-03-26 16:52:16 -0400

    added a function for isCapital, this analyses if the letter is capital or not

--- CaeserCypher.js
+++ CaeserCypher.js
@@ -2,6 +2,22 @@ alert("The CaeserCypher is working Sire");
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
+// function isCapital made because we have to have a code that determines 
+// if letters are Capital or not.
+
+function isCapital(letter) {
+    if (alphabet.indexOf(letter.toLowerCase()) !== alphabet.indexOf(letter)) {
+        return true;
+    }
+    else {
+        return false; 
+    }
+}
+
+
+
+
+
 function encrypt (message, shiftValue)
 {
   // Your encryption code here
