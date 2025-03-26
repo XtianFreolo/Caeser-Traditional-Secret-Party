14fbe4531fb7e883b3adb596241fe2d08d285d02
XtianFreolo <f60@umbc.edu>
2025-03-26 16:43:51 -0400

    This is the start of the CaeserCypher activity

---
+++ CaeserCypher.js
@@ -0,0 +1,16 @@
+alert("The CaeserCypher is working Sire");
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+function encrypt (message, shiftValue)
+{
+  // Your encryption code here
+  return encryptedMessage;
+}
+
+function decrypt (encryptedMessage, shiftValue)
+{
+  // Your decryption code here
+  return decryptedMessage;
+}
+
