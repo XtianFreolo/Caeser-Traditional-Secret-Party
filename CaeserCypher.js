5628ada0120e500fdf04aeafdc65afecfdfe0666
XtianFreolo <f60@umbc.edu>
2025-03-26 17:02:18 -0400

    Added an encryptLetter function. This is needed to build encrypt later on. comments are in for the code

--- CaeserCypher.js
+++ CaeserCypher.js
@@ -15,14 +15,43 @@ function isCapital(letter) {
 }
 
 
-
-
-
 function encrypt (message, shiftValue)
 {
   // Your encryption code here
   return encryptedMessage;
 }
+
+function encryptLetter(letter, shift) {
+    const index = alphabet.indexOf(letter.toLowerCase());
+ // this make sures the non-alphabetical integers are returned as is.
+    if (index === -1) {
+        return letter;
+    }
+ // makes sure that the index continues to wrap around after Z
+    const newIndex = (index + shift) % alphabet.length;
+
+ // this makes it that "if the letter is true -- isCapital then return it to uppercase
+ //  if not then return it as normal"
+    if (isCapital(letter)) {
+        return alphabet[newIndex].toUpperCase();
+    }
+
+    else {
+        return alphabet[newIndex];
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 function decrypt (encryptedMessage, shiftValue)
 {
